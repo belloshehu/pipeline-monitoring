a7735418d77e228bb17d4addc8d697714798a0a2
Belloshehu <belloshehu1@gmail.com>
2024-01-02 14:24:33 +0100

    Fix import error

--- src/components/styles/Button.styled.ts
+++ src/components/styles/Button.styled.ts
@@ -1,7 +1,14 @@
 import styled, { WebTarget } from "styled-components";
 import { motion } from "framer-motion";
 
-export const StyledButton = styled(motion.button as WebTarget)`
+export const StyledButton = styled(motion.button as WebTarget).attrs(() => ({
+  // drag: "x",
+  initial: { scale: 0.6 },
+  animate: { scale: 1 },
+  transition: { duration: 0.3 },
+  whileHover: { scale: 1.2 },
+  // dragConstraints: { left: -100, right: 100 },
+}))`
   background: ${({ theme, bgColor }) => bgColor || theme.backgroundColor};
   color: ${({ color, theme }) => color || theme.color};
   padding: 0.6rem 1rem;
